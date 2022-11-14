444b612ccecb18290568537cedc04950269bc79d
Nikita Pushkarskii <11289396+pushkarski@users.noreply.github.com>
2022-11-14 12:14:17 -0500

    adds 1.10: unicafe step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const Header = ({ header }) => <h2>{header}</h2>
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
+)
+
+
+const StatisticLine = ({ text, value }) => (
+  <p>{text} {value}</p>
 )
 
 const Statistics = ({ values }) => {
@@ -12,12 +17,12 @@ const Statistics = ({ values }) => {
     values.badVal > 0) {
       return (
         <>
-          <p>{values.goodStr} {values.goodVal}</p>
-          <p>{values.neutralStr} {values.neutralVal}</p> 
-          <p>{values.badStr} {values.badVal}</p> 
-          <p>{values.allStr} {values.goodVal + values.neutralVal + values.badVal}</p> 
-          <p>{values.avgStr} {(values.goodVal - values.badVal) / 10}</p> 
-          <p>{values.percentStr} {values.goodVal / (values.badVal + values.neutralVal + values.goodVal) * 100}</p> 
+          <StatisticLine text={values.goodStr} value={values.goodVal}/>
+          <StatisticLine text={values.neutralStr} value={values.neutralVal}/>
+          <StatisticLine text={values.badStr} value={values.badVal}/>
+          <StatisticLine text={values.allStr} value={values.goodVal + values.neutralVal + values.badVal}/>
+          <StatisticLine text={values.avgStr} value={(values.goodVal - values.badVal) / 10}/>
+          <StatisticLine text={values.percentStr} value={values.goodVal / (values.badVal + values.neutralVal + values.goodVal) * 100}/>
         </>
       )
   }
@@ -61,11 +66,11 @@ const App = () => {
       <Header header="give feedback"/>
       <Button handleClick={handleGoodClick} text="good"/>
       <Button handleClick={handleNeutralClick} text="neutral"/>
-      <Button handleClick={handleBadClick} text="bad" />
+      <Button handleClick={handleBadClick} text="bad"/>
       
-      <Header header="statistics" />
+      <Header header="statistics"/>
       <Statistics values={values}
-      />
+     />
       
     </>
   );
